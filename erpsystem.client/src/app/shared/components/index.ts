34336037114348345def512a902d646728c8acb9
Sajid Ameen <42244729+Sajidameen2323@users.noreply.github.com>
2025-07-09 14:24:43 +0530

    clear unused

--- erpsystem.client/src/app/shared/components/index.ts
+++ erpsystem.client/src/app/shared/components/index.ts
@@ -5,11 +5,9 @@ export * from './sidebar/sidebar.component';
 
 // UI Components
 export * from './card/card.component';
-export * from './data-table/data-table.component';
 export * from './toast/toast.component';
 
 // Types and Interfaces
 export type { UserProfile, Notification } from './header/header.component';
-export type { TableColumn, TableAction } from './data-table/data-table.component';
 export type { Toast, ToastType } from './toast/toast.component';
 export type { CardAction } from './card/card.component';
